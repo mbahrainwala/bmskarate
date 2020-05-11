b298881dc20251c5d10f5171640bf36fb8efc797
Mustafa Behrainwala <mbahrainwala@hotmail.com>
2020-05-11 05:20:42 -0400

    initial commit

---
+++ src/main/java/ca/bmskarate/bmskarate/BmskarateApplication.java
@@ -0,0 +1,13 @@
+package ca.bmskarate.bmskarate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmskarateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmskarateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/bmskarate/bmskarate/BmskarateApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.bmskarate.bmskarate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmskarateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
